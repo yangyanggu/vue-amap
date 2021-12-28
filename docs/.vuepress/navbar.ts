0a8e629818b44bdddc9094e6d72e0a06d3ccf5c6
guyangyang <407042815@qq.com>
2021-12-28 11:31:29 +0800

    增加github地址

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -1,22 +1,26 @@
 export default [
-    {
-        text: 'vue-bmap-gl',
-        link: 'https://docs.guyixi.cn/vue-bmap-gl/#/'
-    },
-    {
-        text: 'vue-mapvgl',
-        link: 'https://docs.guyixi.cn/vue-mapvgl/#/'
-    },
-    {
-        text: 'vue2版本',
-        link: 'https://docs.guyixi.cn/vue-amap/#/'
-    },
-    {
-        text: '更新日志',
-        link: 'https://gitee.com/guyangyang/vue-amap/blob/dev/CHANGELOG.md'
-    },
-    {
-        text: 'gitee',
-        link: 'https://gitee.com/guyangyang/vue-amap'
-    }
+  {
+    text: 'vue-bmap-gl',
+    link: 'https://docs.guyixi.cn/vue-bmap-gl/#/'
+  },
+  {
+    text: 'vue-mapvgl',
+    link: 'https://docs.guyixi.cn/vue-mapvgl/#/'
+  },
+  {
+    text: 'vue2版本',
+    link: 'https://docs.guyixi.cn/vue-amap/#/'
+  },
+  {
+    text: '更新日志',
+    link: 'https://gitee.com/guyangyang/vue-amap/blob/dev/CHANGELOG.md'
+  },
+  {
+    text: 'gitee',
+    link: 'https://gitee.com/guyangyang/vue-amap'
+  },
+  {
+    text: 'github',
+    link: 'https://github.com/yangyanggu/vue-amap/'
+  }
 ]
