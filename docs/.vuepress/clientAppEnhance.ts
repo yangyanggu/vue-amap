f880877ace37b7c5dc597f2697d83117ac600a65
guyangyang <407042815@qq.com>
2021-12-30 14:35:40 +0800

    修改文档依赖

--- docs/.vuepress/clientAppEnhance.ts
+++ docs/.vuepress/clientAppEnhance.ts
@@ -3,7 +3,7 @@ import '@vuemap/vue-amap/dist/style.css'
 
 export default defineClientAppEnhance(({ app }) => {
   if (!__VUEPRESS_SSR__) {
-    import('@vuemap/vue-amap/dist/index.es.js').then(va => {
+    import('@vuemap/vue-amap').then(va => {
       app.use(va);
       va.initAMapApiLoader({
         key: '747f980f217a31ba68d99301045a3fa7'
