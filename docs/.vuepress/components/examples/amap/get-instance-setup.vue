b0cc452fa09da9969b7e9cbe1140622b351f818c
guyangyang <407042815@qq.com>
2022-01-27 14:17:51 +0800

    修改文档，修改获取ref的方法

--- docs/.vuepress/components/examples/amap/get-instance-setup.vue
+++ docs/.vuepress/components/examples/amap/get-instance-setup.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="map-page-container">
-    <el-amap ref="map" :min-zoom="10" :max-zoom="22" :center="center" :zoom="zoom" @init="init" @click="click">
+    <el-amap ref="mapRef" :min-zoom="10" :max-zoom="22" :center="center" :zoom="zoom" @init="init" @click="click">
     </el-amap>
   </div>
   <div class="toolbar">
@@ -11,18 +11,18 @@
 import {ref, getCurrentInstance} from 'vue';
 
 const {proxy} = getCurrentInstance();
-
+const mapRef = ref();
 const zoom = ref(16);
 const center = ref([121.59996, 31.197646]);
 let map = null;
 const getMap = () => {
   // bmap vue component
-  console.log('setup $refs: ', proxy.$refs.map.$$getInstance())
+  console.log('setup $refs: ', mapRef.value.$$getInstance())
 };
 const init = (o) => {
   map = o;
   console.log('setup center: ', o.getCenter())
-  console.log('setup init $refs: ', proxy.$refs.map.$$getInstance())
+  console.log('setup init $refs: ', mapRef.value.$$getInstance())
 }
 const click = () => {
   alert('click map')
