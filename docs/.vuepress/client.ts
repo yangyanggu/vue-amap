93a99740485232753032464e4af557b513cc84e0
gyy <407042815@qq.com>
2023-09-16 20:50:57 +0800

    调整文档打包方式

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,16 +1,14 @@
 import {defineClientConfig} from '@vuepress/client'
-import '@vuemap/vue-amap/dist/style.css'
-
+import * as VueAMap from '@vuemap/vue-amap/index'
+import * as VueAMapLoca from '@vuemap/vue-amap-loca/index'
+import * as VueAMapExtra from '@vuemap/vue-amap-extra/index'
 export default defineClientConfig({
-  async enhance({app}) {
+  enhance({app}) {
     if (!__VUEPRESS_SSR__) {
-      const va = await import('@vuemap/vue-amap');
-      const loca = await import('@vuemap/vue-amap-loca');
-      const extra = await import('@vuemap/vue-amap-extra');
-      app.use(va);
-      app.use(loca);
-      app.use(extra);
-      va.initAMapApiLoader({
+      app.use(VueAMap);
+      app.use(VueAMapLoca);
+      app.use(VueAMapExtra);
+      VueAMap.initAMapApiLoader({
         key: '131de8dce165c06abe06564c351099f3',
         securityJsCode: 'e2ef604bb7b4fecdc69bacd5c2400b6f',
         Loca: {
