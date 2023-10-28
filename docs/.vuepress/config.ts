64483840fb302c462a42cd5dcc5467d3e262df6f
gyy <407042815@qq.com>
2023-10-28 20:39:53 +0800

    移除百度统计、文档搜索

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,5 +1,4 @@
 import {defineUserConfig} from 'vuepress'
-import {docsearchPlugin} from "@vuepress/plugin-docsearch";
 import Navbar from './navbar'
 import Sidebar from './sidebar'
 import registerPlugin from "./plugin/registerPlugin";
@@ -16,14 +15,6 @@ export default defineUserConfig({
   description: '高德地图2.0版本的vue3对应封装',
   head: [
     ['link', {rel: 'stylesheet', type: 'text/css' ,href: `${basePath}css/global.css`}],
-    ['script', {}, `var _hmt = _hmt || [];
-      (function() {
-        var hm = document.createElement("script");
-        hm.src = "https://hm.baidu.com/hm.js?3531c3641194434fc0076c594b820067";
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s);
-      })();`
-    ]
     // ['script', {src: 'https://cdn.jsdelivr.net/npm/@vuemap/vue-amap@1.0.1/dist/index.js'}]
   ],
 
@@ -42,11 +33,6 @@ export default defineUserConfig({
   plugins: [
     containerPlugin(),
     registerPlugin,
-    sitemapPlugin(),
-    docsearchPlugin({
-      apiKey: '45d05a8e842f2a0869d1fe5b60c67d7f',
-      indexName: 'vue-amap-guyixi',
-      appId: 'BSHGEQA36W'
-    })
+    sitemapPlugin()
   ],
 })
