89f842c7e0ac5860a941b0b641dde85f15fc4ab5
gyy <407042815@qq.com>
2024-02-01 23:48:47 +0800

    修改 DistrictCluster

--- src/vue-amap/packages/layer/data/DistrictCluster/DistrictCluster.vue
+++ src/vue-amap/packages/layer/data/DistrictCluster/DistrictCluster.vue
@@ -1,54 +1,66 @@
-<script lang="ts">
+<template>
+  <div />
+</template>
+<script setup lang="ts">
 import './addProcess';
-import {defineComponent} from "vue";
+import {defineOptions} from 'vue';
 import {DistrictCluster} from "@vuemap/district-cluster";
-import {registerMixin} from '@vuemap/vue-amap';
-import type { PropType } from 'vue';
+import {useRegister} from "../../../../mixins";
+import {buildProps} from "../../../../utils/buildHelper";
+import type {PropType} from 'vue';
 import type { RenderOptions } from '@vuemap/district-cluster';
 type GetPosition = (dataItem: any, dataIndex: number) => [number, number]
 
-export default defineComponent({
+defineOptions({
   name: 'ElAmapLayerDistrictCluster',
-  mixins: [registerMixin],
-  props: {
-    data: {
-      required: true,
-      type: Array
-    }, // 数据源数组，每个元素即为点相关的信息
-    getPosition: {
-      type: Function as PropType<GetPosition>
-    }, // 返回数据项中的经纬度信息
-    autoSetFitView: {
-      type: Boolean,
-      default: true
-    }, // 是否在绘制后自动调整地图视野以适合全部点，默认true
-    topAdcodes: {
-      type: Array as PropType<Array<number>>
-    },// 顶层区划的adcode列表
-    excludedAdcodes: {
-      type: Array as PropType<Array<number>>
-    },
-    renderOptions: {
-      type: Object as PropType<RenderOptions>
-    } // 绘制的引擎的参数
+  inheritAttrs: false
+});
+
+defineProps(buildProps({
+  data: {
+    required: true,
+    type: Array
+  }, // 数据源数组，每个元素即为点相关的信息
+  getPosition: {
+    type: Function as PropType<GetPosition>
+  }, // 返回数据项中的经纬度信息
+  autoSetFitView: {
+    type: Boolean,
+    default: true
+  }, // 是否在绘制后自动调整地图视野以适合全部点，默认true
+  topAdcodes: {
+    type: Array as PropType<Array<number>>
+  },// 顶层区划的adcode列表
+  excludedAdcodes: {
+    type: Array as PropType<Array<number>>
   },
-  data() {
-    return {
-    };
-  },
-  methods: {
-    __initComponent(options) {
-      options.map = this.$parentComponent;
-      this.$amapComponent = new DistrictCluster(options);
-    },
-    destroyComponent() {
-      this.$amapComponent.destroy();
-      this.$amapComponent = null;
-      this.$parentComponent = null;
+  renderOptions: {
+    type: Object as PropType<RenderOptions>
+  } // 绘制的引擎的参数
+}));
+const emits = defineEmits(['init']);
+
+let $amapComponent: DistrictCluster;
+
+const {$$getInstance} = useRegister<DistrictCluster, AMap.Map>((options, parentComponent) => {
+  return new Promise<DistrictCluster>((resolve) => {
+    options.map = parentComponent;
+    $amapComponent = new DistrictCluster(options);
+    resolve($amapComponent);
+  });
+
+}, {
+  emits,
+  destroyComponent () {
+    if ($amapComponent) {
+      $amapComponent.destroy();
+      $amapComponent = null as any;
     }
   },
-  render(){
-    return null;
-  }
 });
+
+defineExpose({
+  $$getInstance
+});
+
 </script>
--- src/vue-amap/packages/layer/data/DistrictCluster/index.ts
+++ src/vue-amap/packages/layer/data/DistrictCluster/index.ts
@@ -1,8 +1,10 @@
-import DistrictCluster from './DistrictCluster.vue'
+import DistrictCluster from './DistrictCluster.vue';
 import type {Plugin, App} from "vue";
 DistrictCluster.install = (app: App) => {
-  app.component(DistrictCluster.name, DistrictCluster)
-  return app
-}
-export const ElAmapLayerDistrictCluster = DistrictCluster as typeof DistrictCluster & Plugin
-export default ElAmapLayerDistrictCluster
+  app.component(DistrictCluster.name, DistrictCluster);
+  return app;
+};
+export const ElAmapLayerDistrictCluster = DistrictCluster as typeof DistrictCluster & Plugin;
+export default ElAmapLayerDistrictCluster;
+
+export type ElAmapLayerDistrictClusterInstance = InstanceType<typeof DistrictCluster>
