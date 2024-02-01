0688137c5c5a9fbb4e110ca299c50c0354e9df97
gyy <407042815@qq.com>
2024-02-01 22:30:00 +0800

    修改 Scale

--- src/vue-amap/packages/control/Scale/index.ts
+++ src/vue-amap/packages/control/Scale/index.ts
@@ -1,8 +1,10 @@
-import Scale from './Scale.vue'
+import Scale from './Scale.vue';
 import type {Plugin, App} from "vue";
 Scale.install = (app: App) => {
-  app.component(Scale.name, Scale)
-  return app
-}
-export const ElAmapControlScale = Scale as typeof Scale & Plugin
-export default ElAmapControlScale
+  app.component(Scale.name, Scale);
+  return app;
+};
+export const ElAmapControlScale = Scale as typeof Scale & Plugin;
+export default ElAmapControlScale;
+
+export type ElAmapControlScaleInstance = InstanceType<typeof ElAmapControlScale>
