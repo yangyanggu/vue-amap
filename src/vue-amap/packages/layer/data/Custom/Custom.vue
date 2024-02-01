0c36f56f907d3d3f15087271973f5a865c093ffb
gyy <407042815@qq.com>
2024-02-01 23:40:02 +0800

    修改 Custom

--- src/vue-amap/packages/layer/data/Custom/Custom.vue
+++ src/vue-amap/packages/layer/data/Custom/Custom.vue
@@ -1,48 +1,61 @@
-<script lang="ts">
-import {defineComponent} from "vue";
-import {registerMixin} from '@vuemap/vue-amap';
+<template>
+  <div />
+</template>
+<script setup lang="ts">
+import {defineOptions} from 'vue';
+import {useRegister} from "../../../../mixins";
+import {buildProps} from "../../../../utils/buildHelper";
+import type {PropType} from 'vue';
 
-export default defineComponent({
+defineOptions({
   name: 'ElAmapLayerCustom',
-  mixins: [registerMixin],
-  props: {
-    canvas: {
-      required: true,
-      type: Object
-    }, // canvas 对象
-    render: {
-      type: Function
-    }, // 绘制函数，初始化完成时候，开发者需要给该图层设定render方法，该方法需要实现图层的绘制，API会在合适的时机自动调用该方法
-    zooms: {
-      type: Array
-    }, // 支持的缩放级别范围，默认范围 [2, 20]
-    opacity: {
-      type: Number
-    }, // 透明度，默认 1
-    alwaysRender: {
-      type: Boolean,
-      default: false
-    }// 是否主动
-  },
-  data() {
-    return {
-    };
-  },
-  methods: {
-    __initComponent(options) {
-      const canvas = options.canvas;
-      delete options.canvas;
-      this.$amapComponent = new AMap.CustomLayer(canvas, options);
-      this.$amapComponent.setMap(this.$parentComponent);
-    },
-    destroyComponent() {
-      this.$amapComponent.setMap(null);
-      this.$amapComponent = null;
-      this.$parentComponent = null;
+  inheritAttrs: false
+});
+
+defineProps(buildProps({
+  canvas: {
+    required: true,
+    type: Object
+  }, // canvas 对象
+  render: {
+    type: Function
+  }, // 绘制函数，初始化完成时候，开发者需要给该图层设定render方法，该方法需要实现图层的绘制，API会在合适的时机自动调用该方法
+  zooms: {
+    type: Array
+  }, // 支持的缩放级别范围，默认范围 [2, 20]
+  opacity: {
+    type: Number
+  }, // 透明度，默认 1
+  alwaysRender: {
+    type: Boolean,
+    default: false
+  }// 是否主动
+}));
+const emits = defineEmits(['init']);
+
+let $amapComponent: AMap.CustomLayer;
+
+const {$$getInstance, parentInstance} = useRegister<AMap.CustomLayer, AMap.Map>((options, parentComponent) => {
+  return new Promise<AMap.CustomLayer>((resolve) => {
+    const canvas = options.canvas;
+    delete options.canvas;
+    $amapComponent = new AMap.CustomLayer(canvas, options);
+    $amapComponent.setMap(parentComponent);
+    resolve($amapComponent);
+  });
+
+}, {
+  emits,
+  destroyComponent () {
+    if ($amapComponent && parentInstance?.$amapComponent) {
+      $amapComponent.setMap(null);
+      $amapComponent = null as any;
     }
   },
-  render(){
-    return null;
-  }
 });
+
+defineExpose({
+  $$getInstance
+});
+
 </script>
--- src/vue-amap/packages/layer/data/Custom/index.ts
+++ src/vue-amap/packages/layer/data/Custom/index.ts
@@ -1,8 +1,10 @@
-import Custom from './Custom.vue'
+import Custom from './Custom.vue';
 import type {Plugin, App} from "vue";
 Custom.install = (app: App) => {
-  app.component(Custom.name, Custom)
-  return app
-}
-export const ElAmapLayerCustom = Custom as typeof Custom & Plugin
-export default ElAmapLayerCustom
+  app.component(Custom.name, Custom);
+  return app;
+};
+export const ElAmapLayerCustom = Custom as typeof Custom & Plugin;
+export default ElAmapLayerCustom;
+
+export type ElAmapLayerCustomInstance = InstanceType<typeof Custom>
