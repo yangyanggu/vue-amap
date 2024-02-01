cc9d56e73e2c3a5cf775c7c1d4a2c64031606cbd
gyy <407042815@qq.com>
2024-02-01 23:55:25 +0800

    修改 GLCustom

--- src/vue-amap/packages/layer/data/GLCustom/GLCustom.vue
+++ src/vue-amap/packages/layer/data/GLCustom/GLCustom.vue
@@ -1,41 +1,53 @@
-<script lang="ts">
-import {defineComponent} from "vue";
-import {registerMixin} from '@vuemap/vue-amap';
+<template>
+  <div />
+</template>
+<script setup lang="ts">
+import {defineOptions} from 'vue';
+import {useRegister} from "../../../../mixins";
+import {buildProps} from "../../../../utils/buildHelper";
 
-export default defineComponent({
+defineOptions({
   name: 'ElAmapLayerGlCustom',
-  mixins: [registerMixin],
-  props: {
-    init: {
-      type: Function
-    }, // 初始化的时候，开发者可以在这个函数参数里面获取 gl 上下文，进行一些初始化的操作。
-    render: {
-      type: Function
-    }, // 绘制函数，初始化完成时候，开发者需要给该图层设定render方法，该方法需要实现图层的绘制，API会在合适的时机自动调用该方法
-    zooms: {
-      type: Array
-    }, // 支持的缩放级别范围，默认范围 [2, 20]
-    opacity: {
-      type: Number
-    } // 透明度，默认 1
-  },
-  data() {
-    return {
-    };
-  },
-  methods: {
-    __initComponent(options) {
-      this.$amapComponent = new AMap.GLCustomLayer(options);
-      this.$amapComponent.setMap(this.$parentComponent);
-    },
-    destroyComponent() {
-      this.$amapComponent.setMap(null);
-      this.$amapComponent = null;
-      this.$parentComponent = null;
+  inheritAttrs: false
+});
+
+defineProps(buildProps({
+  init: {
+    type: Function
+  }, // 初始化的时候，开发者可以在这个函数参数里面获取 gl 上下文，进行一些初始化的操作。
+  render: {
+    type: Function
+  }, // 绘制函数，初始化完成时候，开发者需要给该图层设定render方法，该方法需要实现图层的绘制，API会在合适的时机自动调用该方法
+  zooms: {
+    type: Array
+  }, // 支持的缩放级别范围，默认范围 [2, 20]
+  opacity: {
+    type: Number
+  } // 透明度，默认 1
+});
+const emits = defineEmits(['init']);
+
+let $amapComponent: AMap.GLCustomLayer;
+
+const {$$getInstance} = useRegister<AMap.GLCustomLayer, AMap.Map>((options, parentComponent) => {
+  return new Promise<AMap.GLCustomLayer>((resolve) => {
+    $amapComponent = new AMap.GLCustomLayer(options);
+    $amapComponent.setMap(parentComponent);
+    resolve($amapComponent);
+  });
+
+}, {
+  emits,
+  destroyComponent () {
+    if ($amapComponent) {
+      $amapComponent.setMap(null)
+      $amapComponent = null as any;
     }
   },
-  render(){
-    return null;
-  }
 });
+
+defineExpose({
+  $$getInstance
+});
+
 </script>
--- src/vue-amap/packages/layer/data/GLCustom/index.ts
+++ src/vue-amap/packages/layer/data/GLCustom/index.ts
@@ -1,8 +1,10 @@
-import GLCustom from './GLCustom.vue'
+import GLCustom from './GLCustom.vue';
 import type {Plugin, App} from "vue";
 GLCustom.install = (app: App) => {
-  app.component(GLCustom.name, GLCustom)
-  return app
-}
-export const ElAmapLayerGlCustom = GLCustom as typeof GLCustom & Plugin
-export default ElAmapLayerGlCustom
+  app.component(GLCustom.name, GLCustom);
+  return app;
+};
+export const ElAmapLayerGlCustom = GLCustom as typeof GLCustom & Plugin;
+export default ElAmapLayerGlCustom;
+
+export type ElAmapLayerGlCustomInstance = InstanceType<typeof GLCustom>
