8de48da7724cc481a5884315ca8ef89cb1c0c718
gyy <407042815@qq.com>
2024-02-01 23:58:59 +0800

    修改 HeatMap

--- src/vue-amap/packages/layer/data/HeatMap/HeatMap.vue
+++ src/vue-amap/packages/layer/data/HeatMap/HeatMap.vue
@@ -1,61 +1,68 @@
-<script lang="ts">
-import {defineComponent} from "vue";
-import {registerMixin} from '@vuemap/vue-amap';
+<template>
+  <div />
+</template>
+<script setup lang="ts">
+import {defineOptions} from 'vue';
+import {useRegister} from "../../../../mixins";
+import {buildProps} from "../../../../utils/buildHelper";
+import type {PropType} from 'vue';
 
-export default defineComponent({
+defineOptions({
   name: 'ElAmapLayerHeatMap',
-  mixins: [registerMixin],
-  props: {
-    radius: {
-      type: Number
-    }, // 热力图中单个点的半径，默认：30，单位：pixel
-    gradient: {
-      type: Object
-    }, // 热力图的渐变区间，热力图按照设置的颜色及间隔显示热力图，例{0.4:'rgb(0, 255, 255)',0.85:'rgb(100, 0, 255)',},其中 key 表示间隔位置，取值范围： [0,1] ，value 为颜色值。默认：heatmap.js标准配色方案
-    zooms: {
-      type: Array
-    }, // 支持的缩放级别范围，默认范围 [2-30]
-    opacity: {
-      type: Array
-    }, // 热力图透明度区间数组，取值范围 [0,1] ，0表示完全透明，1表示不透明，默认： [0,1]
-    config: {
-      type: Object
-    }, // 3D热力图属性
-    dataSet: {
-      type: Object
-    }// 热力图数据集
-  },
-  data() {
-    return {
-      propsRedirect: {
-        config: '3d'
+  inheritAttrs: false
+});
+
+const props = defineProps(buildProps({
+  radius: {
+    type: Number
+  }, // 热力图中单个点的半径，默认：30，单位：pixel
+  gradient: {
+    type: Object as PropType<Record<number, string>>
+  }, // 热力图的渐变区间，热力图按照设置的颜色及间隔显示热力图，例{0.4:'rgb(0, 255, 255)',0.85:'rgb(100, 0, 255)',},其中 key 表示间隔位置，取值范围： [0,1] ，value 为颜色值。默认：heatmap.js标准配色方案
+  zooms: {
+    type: Array
+  }, // 支持的缩放级别范围，默认范围 [2-30]
+  opacity: {
+    type: Object as PropType<[number, number]>
+  }, // 热力图透明度区间数组，取值范围 [0,1] ，0表示完全透明，1表示不透明，默认： [0,1]
+  config: {
+    type: Object
+  }, // 3D热力图属性
+  dataSet: {
+    type: Object
+  }// 热力图数据集
+}));
+const emits = defineEmits(['init']);
+
+let $amapComponent: AMap.HeatMap;
+
+const {$$getInstance, parentInstance} = useRegister<AMap.HeatMap, AMap.Map>((options, parentComponent) => {
+  return new Promise<AMap.CanvasLayer>((resolve) => {
+    AMap.plugin(['AMap.HeatMap'], () => {
+      delete options.dataSet;
+      $amapComponent = new AMap.HeatMap(parentComponent, options);
+      if (props.dataSet) {
+        $amapComponent.setDataSet(props.dataSet);
       }
-    };
-  },
-  methods: {
-    __initComponent(options) {
-      return new Promise<void>((resolve) => {
-        AMap.plugin(['AMap.HeatMap'], () => {
-          delete options.dataSet;
-          this.$amapComponent = new AMap.HeatMap(this.$parentComponent, options);
-          if (this.dataSet) {
-            this.$amapComponent.setDataSet(this.dataSet);
-          }
-          resolve();
-        });
-      });
-    },
-    destroyComponent() {
-      this.$amapComponent.setDataSet({
+      resolve($amapComponent);
+    });
+  });
+
+}, {
+  emits,
+  destroyComponent () {
+    if ($amapComponent && parentInstance?.$amapComponent) {
+      $amapComponent.setDataSet({
         data: []
       });
-      this.$amapComponent.setMap(null);
-      this.$amapComponent = null;
-      this.$parentComponent = null;
+      $amapComponent.setMap(null);
+      $amapComponent = null as any;
     }
   },
-  render(){
-    return null;
-  }
 });
+
+defineExpose({
+  $$getInstance
+});
+
 </script>
--- src/vue-amap/packages/layer/data/HeatMap/index.ts
+++ src/vue-amap/packages/layer/data/HeatMap/index.ts
@@ -1,8 +1,10 @@
-import HeatMap from './HeatMap.vue'
+import HeatMap from './HeatMap.vue';
 import type {Plugin, App} from "vue";
 HeatMap.install = (app: App) => {
-  app.component(HeatMap.name, HeatMap)
-  return app
-}
-export const ElAmapLayerHeatMap = HeatMap as typeof HeatMap & Plugin
-export default ElAmapLayerHeatMap
+  app.component(HeatMap.name, HeatMap);
+  return app;
+};
+export const ElAmapLayerHeatMap = HeatMap as typeof HeatMap & Plugin;
+export default ElAmapLayerHeatMap;
+
+export type ElAmapLayerHeatMapInstance = InstanceType<typeof HeatMap>
