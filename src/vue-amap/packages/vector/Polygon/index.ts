9f5993ff9bddf2696cf32634b415df979fde09d2
gyy <407042815@qq.com>
2024-02-02 23:00:23 +0800

    修改 Polygon

--- src/vue-amap/packages/vector/Polygon/index.ts
+++ src/vue-amap/packages/vector/Polygon/index.ts
@@ -1,8 +1,10 @@
-import Polygon from './Polygon.vue'
+import Polygon from './Polygon.vue';
 import type {Plugin, App} from "vue";
 Polygon.install = (app: App) => {
-  app.component(Polygon.name, Polygon)
-  return app
-}
-export const ElAmapPolygon = Polygon as typeof Polygon & Plugin
-export default ElAmapPolygon
+  app.component(Polygon.name, Polygon);
+  return app;
+};
+export const ElAmapPolygon = Polygon as typeof Polygon & Plugin;
+export default ElAmapPolygon;
+
+export type ElAmapPolygonInstance = InstanceType<typeof Polygon>
