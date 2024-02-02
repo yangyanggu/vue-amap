bd623b757bdac5d2eadd7f5995a8682b547a387f
gyy <407042815@qq.com>
2024-02-02 22:45:15 +0800

    修改 Ellipse

--- src/vue-amap/packages/vector/Ellipse/index.ts
+++ src/vue-amap/packages/vector/Ellipse/index.ts
@@ -1,8 +1,10 @@
-import Ellipse from './Ellipse.vue'
+import Ellipse from './Ellipse.vue';
 import type {Plugin, App} from "vue";
 Ellipse.install = (app: App) => {
-  app.component(Ellipse.name, Ellipse)
-  return app
-}
-export const ElAmapEllipse = Ellipse as typeof Ellipse & Plugin
-export default ElAmapEllipse
+  app.component(Ellipse.name, Ellipse);
+  return app;
+};
+export const ElAmapEllipse = Ellipse as typeof Ellipse & Plugin;
+export default ElAmapEllipse;
+
+export type ElAmapEllipseInstance = InstanceType<typeof Ellipse>
