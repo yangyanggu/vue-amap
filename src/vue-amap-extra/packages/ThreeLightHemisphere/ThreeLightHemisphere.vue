682835c4fdb89af3fd61232ad726b5217ed08ff3
gyy <407042815@qq.com>
2024-02-03 17:42:36 +0800

    修改 ThreeLightHemisphere

--- src/vue-amap-extra/packages/ThreeLightHemisphere/ThreeLightHemisphere.vue
+++ src/vue-amap-extra/packages/ThreeLightHemisphere/ThreeLightHemisphere.vue
@@ -1,47 +1,61 @@
-<script lang="ts">
-import {defineComponent} from "vue";
-import {registerMixin} from '@vuemap/vue-amap';
+<template>
+  <div />
+</template>
+<script setup lang="ts">
+import {defineOptions, getCurrentInstance} from 'vue';
+import {useRegister, buildProps} from "@vuemap/vue-amap";
 import ThreeLightHemisphere from "./ThreeLightHemisphere";
 import type { PropType} from "vue";
 import type {Vec} from "./Type";
 
-export default defineComponent({
+defineOptions({
   name: 'ElAmapThreeLightHemisphere',
-  mixins: [registerMixin],
-  props: {
-    color: {
-      type: String,
-      default: '#ffffff'
-    },
-    groundColor: {
-      type: String,
-      default: '#ffffff'
-    },
-    intensity: {
-      type: Number,
-      default: 1
-    },
-    position: {
-      type: Object as PropType<Vec>,
-      required: true
+  inheritAttrs: false
+});
+
+defineProps(buildProps({
+  color: {
+    type: String,
+    default: '#ffffff'
+  },
+  groundColor: {
+    type: String,
+    default: '#ffffff'
+  },
+  intensity: {
+    type: Number,
+    default: 1
+  },
+  position: {
+    type: Object as PropType<Vec>,
+    required: true
+  }
+}));
+const emits = defineEmits(['init']);
+
+let $amapComponent: ThreeLightHemisphere;
+
+const {$$getInstance, parentInstance} = useRegister<ThreeLightHemisphere, any>((options, parentComponent) => {
+  return new Promise<ThreeLightHemisphere>((resolve) => {
+    $amapComponent = new ThreeLightHemisphere(parentComponent, options);
+    resolve($amapComponent);
+  });
+
+}, {
+  emits,
+  destroyComponent () {
+    if ($amapComponent && parentInstance?.$amapComponent) {
+      if(!parentInstance.isDestroy){
+        $amapComponent.remove();
+      }
+      $amapComponent.destroy();
+      $amapComponent = null as any;
     }
   },
-  data() {
-    return {};
-  },
-  methods: {
-    __initComponent(options) {
-      this.$amapComponent = new ThreeLightHemisphere(this.$parentComponent, options);
-    },
-    destroyComponent() {
-      if(!this.parentInstance.isDestroy){
-        this.$amapComponent.remove();
-      }
-      this.$amapComponent.destroy();
-    },
-  },
-  render() {
-    return null;
-  }
 });
+
+defineExpose({
+  $$getInstance,
+});
+
 </script>
--- src/vue-amap-extra/packages/ThreeLightHemisphere/index.ts
+++ src/vue-amap-extra/packages/ThreeLightHemisphere/index.ts
@@ -1,8 +1,10 @@
-import ThreeLightHemisphere from './ThreeLightHemisphere.vue'
+import ThreeLightHemisphere from './ThreeLightHemisphere.vue';
 import type {Plugin, App} from "vue";
 ThreeLightHemisphere.install = (app: App) => {
-  app.component(ThreeLightHemisphere.name, ThreeLightHemisphere)
-  return app
-}
-export const ElAmapThreeLightHemisphere = ThreeLightHemisphere as typeof ThreeLightHemisphere & Plugin
-export default ElAmapThreeLightHemisphere
+  app.component(ThreeLightHemisphere.name, ThreeLightHemisphere);
+  return app;
+};
+export const ElAmapThreeLightHemisphere = ThreeLightHemisphere as typeof ThreeLightHemisphere & Plugin;
+export default ElAmapThreeLightHemisphere;
+
+export type ElAmapThreeLightHemisphereInstance = InstanceType<typeof ThreeLightHemisphere>
