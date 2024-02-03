7d6ee43835b5064f5ddc9c927f4a367572c5e5b3
gyy <407042815@qq.com>
2024-02-03 17:35:57 +0800

    修改 ThreeGltf

--- src/vue-amap-extra/packages/ThreeGltf/ThreeGltf.vue
+++ src/vue-amap-extra/packages/ThreeGltf/ThreeGltf.vue
@@ -1,74 +1,88 @@
-<script lang="ts">
-import {defineComponent} from "vue";
-import {registerMixin} from '@vuemap/vue-amap';
+<template>
+  <div />
+</template>
+<script setup lang="ts">
+import {defineOptions, getCurrentInstance} from 'vue';
+import {useRegister, buildProps} from "@vuemap/vue-amap";
 import CustomThreeGltf from "./CustomThreeGltf";
 import type {MoveAnimation, Vec, ConfigLoader} from "./Type";
 import type{PropType} from "vue";
 
-export default defineComponent({
+defineOptions({
   name: 'ElAmapThreeGltf',
-  mixins: [registerMixin],
-  props: {
-    url: {
-      type: String
-    },
-    position: {
-      type: Array as PropType<number[]>
-    },
-    height: {
-      type: Number,
-      default: 0
-    },
-    rotation: {
-      type: Object as PropType<Vec>
-    },
-    scale: {
-      type: [Number, Array],
-      default: 1
-    },
-    angle: {
-      type: Number,
-      default: 0
-    },
-    moveAnimation: {
-      type: Object as PropType<MoveAnimation>
-    }, //位置移动是否开启动画
-    configLoader: {
-      type: Function as PropType<ConfigLoader>
-    },
-    useModelCache: {
-      type: Boolean,
-      default: false
-    }
-  },
-  emits: ['click', 'mousemove', 'mouseover', 'mouseout'],
-  data() {
-    return {};
-  },
-  methods: {
-    __initComponent(options) {
-      return new Promise<void>(resolve => {
-        this.$amapComponent = new CustomThreeGltf(this.$parentComponent, options, this, () => {
-          resolve();
-        });
-      });
-
-    },
-    destroyComponent() {
-      if(!this.parentInstance.isDestroy){
-        this.$amapComponent.remove();
-      }
-      this.$amapComponent.destroy();
-    },
-    $$startAnimations() {
-      this.$amapComponent.startAnimations();
-    },
-    $$stopAnimations() {
-      this.$amapComponent.stopAnimations();
-    }
-  },
-  render() {
-    return null;
-  }
+  inheritAttrs: false
 });
+
+defineProps(buildProps({
+  url: {
+    type: String
+  },
+  position: {
+    type: Array as PropType<number[]>
+  },
+  height: {
+    type: Number,
+    default: 0
+  },
+  rotation: {
+    type: Object as PropType<Vec>
+  },
+  scale: {
+    type: [Number, Array],
+    default: 1
+  },
+  angle: {
+    type: Number,
+    default: 0
+  },
+  moveAnimation: {
+    type: Object as PropType<MoveAnimation>
+  }, //位置移动是否开启动画
+  configLoader: {
+    type: Function as PropType<ConfigLoader>
+  },
+  useModelCache: {
+    type: Boolean,
+    default: false
+  }
+}));
+const emits = defineEmits(['init','click', 'mousemove', 'mouseover', 'mouseout']);
+
+let $amapComponent: CustomThreeGltf;
+
+const currentInstance = getCurrentInstance();
+
+const {$$getInstance, parentInstance} = useRegister<CustomThreeGltf, any>((options, parentComponent) => {
+  return new Promise<CustomThreeGltf>((resolve) => {
+    $amapComponent = new CustomThreeGltf(parentComponent, options,currentInstance, () => {
+      resolve($amapComponent);
+    });
+  });
+
+}, {
+  emits,
+  destroyComponent () {
+    if ($amapComponent && parentInstance?.$amapComponent) {
+      if(!parentInstance.isDestroy){
+        $amapComponent.remove();
+      }
+      $amapComponent.destroy();
+      $amapComponent = null as any;
+    }
+  },
+});
+
+const $$startAnimations = () => {
+  $amapComponent.startAnimations();
+};
+const $$stopAnimations = () => {
+  $amapComponent.stopAnimations();
+};
+
+defineExpose({
+  $$getInstance,
+  $$startAnimations,
+  $$stopAnimations,
+});
+
 </script>
--- src/vue-amap-extra/packages/ThreeGltf/index.ts
+++ src/vue-amap-extra/packages/ThreeGltf/index.ts
@@ -1,8 +1,10 @@
-import ThreeGltf from './ThreeGltf.vue'
+import ThreeGltf from './ThreeGltf.vue';
 import type {Plugin, App} from "vue";
 ThreeGltf.install = (app: App) => {
-  app.component(ThreeGltf.name, ThreeGltf)
-  return app
-}
-export const ElAmapThreeGltf = ThreeGltf as typeof ThreeGltf & Plugin
-export default ElAmapThreeGltf
+  app.component(ThreeGltf.name, ThreeGltf);
+  return app;
+};
+export const ElAmapThreeGltf = ThreeGltf as typeof ThreeGltf & Plugin;
+export default ElAmapThreeGltf;
+
+export type ElAmapThreeGltfInstance = InstanceType<typeof ThreeGltf>
