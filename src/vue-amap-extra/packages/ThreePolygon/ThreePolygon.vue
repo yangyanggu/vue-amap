b9dd16517f8a018128bfa8f14aa96502aca93bd8
gyy <407042815@qq.com>
2024-02-03 17:49:02 +0800

    修改 ThreePolygon

--- src/vue-amap-extra/packages/ThreePolygon/ThreePolygon.vue
+++ src/vue-amap-extra/packages/ThreePolygon/ThreePolygon.vue
@@ -1,62 +1,76 @@
-<script lang="ts">
-import {defineComponent} from "vue";
-import {registerMixin} from '@vuemap/vue-amap';
+<template>
+  <div />
+</template>
+<script setup lang="ts">
+import {defineOptions} from 'vue';
+import {useRegister, buildProps} from "@vuemap/vue-amap";
 import ThreePolygon from "./ThreePolygon";
 import type {Polygon} from './Type';
 import type {PropType} from "vue";
 
-export default defineComponent({
+defineOptions({
   name: 'ElAmapThreePolygon',
-  mixins: [registerMixin],
-  props: {
-    sideTopColor: {
-      type: String,
-      default: '#ffffff'
-    },// 侧面顶部颜色
-    sideBottomColor: {
-      type: String,
-      default: '#ffffff'
-    },// 侧面颜色
-    sideTexture: {
-      type: String
-    }, // 侧面贴图
-    topColor: {
-      type: String,
-      default: '#ffffff'
-    },
-    bottomColor: {
-      type: String,
-      default: '#ffffff'
-    },
-    height: {
-      type: Number,
-      default: 30
-    },
-    depthTest: {
-      type: Boolean,
-      default: true
-    },
-    source: {
-      type: Object as PropType<Polygon>,
-      required: true
+  inheritAttrs: false
+});
+
+defineProps(buildProps({
+  sideTopColor: {
+    type: String,
+    default: '#ffffff'
+  },// 侧面顶部颜色
+  sideBottomColor: {
+    type: String,
+    default: '#ffffff'
+  },// 侧面颜色
+  sideTexture: {
+    type: String
+  }, // 侧面贴图
+  topColor: {
+    type: String,
+    default: '#ffffff'
+  },
+  bottomColor: {
+    type: String,
+    default: '#ffffff'
+  },
+  height: {
+    type: Number,
+    default: 30
+  },
+  depthTest: {
+    type: Boolean,
+    default: true
+  },
+  source: {
+    type: Object as PropType<Polygon>,
+    required: true
+  }
+}));
+const emits = defineEmits(['init']);
+
+let $amapComponent: ThreePolygon;
+
+const {$$getInstance, parentInstance} = useRegister<ThreePolygon, any>((options, parentComponent) => {
+  return new Promise<ThreePolygon>((resolve) => {
+    $amapComponent = new ThreePolygon(parentComponent, options);
+    resolve($amapComponent);
+  });
+
+}, {
+  emits,
+  destroyComponent () {
+    if ($amapComponent && parentInstance?.$amapComponent) {
+      if(!parentInstance.isDestroy){
+        $amapComponent.remove();
+      }
+      $amapComponent.destroy();
+      $amapComponent = null as any;
     }
   },
-  data() {
-    return {};
-  },
-  methods: {
-    __initComponent(options) {
-      this.$amapComponent = new ThreePolygon(this.$parentComponent, options);
-    },
-    destroyComponent() {
-      if(!this.parentInstance.isDestroy){
-        this.$amapComponent.remove();
-      }
-      this.$amapComponent.destroy();
-    },
-  },
-  render() {
-    return null;
-  }
 });
+
+defineExpose({
+  $$getInstance,
+});
+
 </script>
--- src/vue-amap-extra/packages/ThreePolygon/index.ts
+++ src/vue-amap-extra/packages/ThreePolygon/index.ts
@@ -1,8 +1,10 @@
-import ThreePolygon from './ThreePolygon.vue'
+import ThreePolygon from './ThreePolygon.vue';
 import type {Plugin, App} from "vue";
 ThreePolygon.install = (app: App) => {
-  app.component(ThreePolygon.name, ThreePolygon)
-  return app
-}
-export const ElAmapThreePolygon = ThreePolygon as typeof ThreePolygon & Plugin
-export default ElAmapThreePolygon
+  app.component(ThreePolygon.name, ThreePolygon);
+  return app;
+};
+export const ElAmapThreePolygon = ThreePolygon as typeof ThreePolygon & Plugin;
+export default ElAmapThreePolygon;
+
+export type ElAmapThreePolygonInstance = InstanceType<typeof ThreePolygon>
