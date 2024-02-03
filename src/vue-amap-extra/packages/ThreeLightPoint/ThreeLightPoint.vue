fd626ade12af54605a45a4f642aaf47ad719760f
gyy <407042815@qq.com>
2024-02-03 17:44:09 +0800

    修改 ThreeLightPoint

--- src/vue-amap-extra/packages/ThreeLightPoint/ThreeLightPoint.vue
+++ src/vue-amap-extra/packages/ThreeLightPoint/ThreeLightPoint.vue
@@ -1,51 +1,65 @@
-<script lang="ts">
-import {defineComponent} from "vue";
-import {registerMixin} from '@vuemap/vue-amap';
+<template>
+  <div />
+</template>
+<script setup lang="ts">
+import {defineOptions, getCurrentInstance} from 'vue';
+import {useRegister, buildProps} from "@vuemap/vue-amap";
 import ThreeLightPoint from "./ThreeLightPoint";
 import type { PropType} from "vue";
 import type {Vec} from "./Type";
 
-export default defineComponent({
+defineOptions({
   name: 'ElAmapThreeLightPoint',
-  mixins: [registerMixin],
-  props: {
-    color: {
-      type: String,
-      default: '#ffffff'
-    },
-    intensity: {
-      type: Number,
-      default: 1
-    },
-    distance: {
-      type: Number,
-      default: 0
-    },
-    decay: {
-      type: Number,
-      default: 1
-    },
-    position: {
-      type: Object as PropType<Vec>,
-      required: true
-    },
-  },
-  data() {
-    return {};
-  },
-  methods: {
-    __initComponent(options) {
-      this.$amapComponent = new ThreeLightPoint(this.$parentComponent, options);
-    },
-    destroyComponent() {
-      if(!this.parentInstance.isDestroy){
-        this.$amapComponent.remove();
-      }
-      this.$amapComponent.destroy();
-    },
-  },
-  render() {
-    return null;
-  }
+  inheritAttrs: false
 });
+
+defineProps(buildProps({
+  color: {
+    type: String,
+    default: '#ffffff'
+  },
+  intensity: {
+    type: Number,
+    default: 1
+  },
+  distance: {
+    type: Number,
+    default: 0
+  },
+  decay: {
+    type: Number,
+    default: 1
+  },
+  position: {
+    type: Object as PropType<Vec>,
+    required: true
+  },
+}));
+const emits = defineEmits(['init']);
+
+let $amapComponent: ThreeLightPoint;
+
+const {$$getInstance, parentInstance} = useRegister<ThreeLightPoint, any>((options, parentComponent) => {
+  return new Promise<ThreeLightPoint>((resolve) => {
+    $amapComponent = new ThreeLightPoint(parentComponent, options);
+    resolve($amapComponent);
+  });
+
+}, {
+  emits,
+  destroyComponent () {
+    if ($amapComponent && parentInstance?.$amapComponent) {
+      if(!parentInstance.isDestroy){
+        $amapComponent.remove();
+      }
+      $amapComponent.destroy();
+      $amapComponent = null as any;
+    }
+  },
+});
+
+defineExpose({
+  $$getInstance,
+});
+
 </script>
--- src/vue-amap-extra/packages/ThreeLightPoint/index.ts
+++ src/vue-amap-extra/packages/ThreeLightPoint/index.ts
@@ -1,8 +1,10 @@
-import ThreeLightPoint from './ThreeLightPoint.vue'
+import ThreeLightPoint from './ThreeLightPoint.vue';
 import type {Plugin, App} from "vue";
 ThreeLightPoint.install = (app: App) => {
-  app.component(ThreeLightPoint.name, ThreeLightPoint)
-  return app
-}
-export const ElAmapThreeLightPoint = ThreeLightPoint as typeof ThreeLightPoint & Plugin
-export default ElAmapThreeLightPoint
+  app.component(ThreeLightPoint.name, ThreeLightPoint);
+  return app;
+};
+export const ElAmapThreeLightPoint = ThreeLightPoint as typeof ThreeLightPoint & Plugin;
+export default ElAmapThreeLightPoint;
+
+export type ElAmapThreeLightPointInstance = InstanceType<typeof ThreeLightPoint>
